4d21dfaf32260c1b2dcafa27a5c5cd3251cb15d6
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-07-06 18:24:02 +0200

    create pinia and mount
    plugin-persistent-state is also added

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import router from "./router/index"
 import { plugin, defaultConfig } from "@formkit/vue"
 import config from "../formkit.config.ts"
 import "@formkit/themes/genesis"
+import { createPinia } from "pinia"
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate"
 
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 const app = createApp(App)
 
-app.use(router).use(plugin, defaultConfig(config)).mount("#app")
+app.use(router).use(plugin, defaultConfig(config)).use(pinia).mount("#app")
