58677379017f4f300bf3a1963edccea9fab7adc3
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-07-07 11:41:44 +0200

    create basic task form

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
+import TaskForm from '../components/TaskForm.vue'
+import { useTaskStore } from '../store/task'
+
+const { tasks, getOpenTasks, closeTask, getFinishedTasks } = useTaskStore()
+
+closeTask(tasks[1])
 </script>
 
 <template>
     <div>Overview page soon to come...</div>
+
+
+    <TaskForm />
+
+    <div>{{ tasks }}</div>
+
+    <div>{{ getOpenTasks }}</div>
+
+    <div>{{ getFinishedTasks }}</div>
 </template>
