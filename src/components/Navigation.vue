4fe9863cf11284ab4d1a278ed0e87a7a671a3ff4
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-07-07 14:44:22 +0200

    add simple navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-card height="75">
+    <v-layout>
+      <v-app-bar height="75" color="light-blue-darken-3">
+        <div class="flex justify-evenly items-center w-full h-full">
+          <RouterLink class="text-lg" to="/">Overview</RouterLink>
+          <RouterLink class="text-lg" to="/finished">Finished Tasks</RouterLink>
+        </div>
+      </v-app-bar>
+    </v-layout>
+  </v-card>
+</template>
