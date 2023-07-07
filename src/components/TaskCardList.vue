a6ee0ed70681e84ef3dcb2a5ca3344a4c23c86b1
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-07-07 14:52:09 +0200

    change color of check mark
    Think it is more logic to have a neutral color for tasks that haven't been done yet, and a positive color for tasks that have been fulfilled

--- src/components/TaskCardList.vue
+++ src/components/TaskCardList.vue
@@ -15,7 +15,7 @@ const { toggleTaskStatus } = useTaskStore()
   <v-sheet v-for="task in tasks" :key="task.text" position="relative" elevation="4" height="100"
     class="d-flex flex-column justify-center items-center relative my-2">
 
-    <v-btn @click="() => toggleTaskStatus(task)" :class="`${task.open ? 'text-green' : 'text-red'} right-2 top-2 w-min`"
+    <v-btn @click="() => toggleTaskStatus(task)" :class="`${task.open ? 'text-gray' : 'text-green'} right-2 top-2 w-min`"
       variant="plain" position="absolute" icon="mdi-check-circle-outline" rounded="xl" />
 
     <p class="text-xl">{{ task.text }}</p>
