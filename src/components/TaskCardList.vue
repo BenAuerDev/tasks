dbf6fc59af5e656e74b3c8699e6c7ace961c6154
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-07-07 13:59:02 +0200

    add basic task-card-list utilizing v-sheet and v-btn

---
+++ src/components/TaskCardList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Task } from '../types/task'
+import { useTaskStore } from '../store/task';
+
+defineProps<{
+  tasks: Task[]
+}>()
+
+
+const { toggleTaskStatus } = useTaskStore()
+
+</script>
+
+<template>
+  <v-sheet v-for="task in tasks" :key="task.text" position="relative" elevation="4" height="100"
+    class="d-flex flex-column justify-center items-center relative my-2">
+
+    <v-btn @click="() => toggleTaskStatus(task)" :class="`${task.open ? 'text-green' : 'text-red'} right-2 top-2 w-min`"
+      variant="plain" position="absolute" icon="mdi-check-circle-outline" rounded="xl" />
+
+    <p class="text-xl">{{ task.text }}</p>
+  </v-sheet>
+</template>
