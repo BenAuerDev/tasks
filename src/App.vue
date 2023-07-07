9d005ff08e286327b008e06d87116b474d0f657d
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-07-07 14:44:40 +0200

    add navigation to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Navigation from './components/Navigation.vue'
 </script>
 
 <template>
+  <Navigation />
   <div class="flex w-full flex-col items-center justify-center">
     <RouterView></RouterView>
   </div>
