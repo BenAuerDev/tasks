750161846a01659e4c1a84ef16820090ef3cebf7
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-07-10 10:52:59 +0200

    add simple fade animation to TaskCardList

--- src/components/TaskCardList.vue
+++ src/components/TaskCardList.vue
@@ -12,12 +12,15 @@ const { toggleTaskStatus } = useTaskStore()
 </script>
 
 <template>
-  <v-sheet v-for="task in tasks" :key="task.text" position="relative" elevation="4" height="100"
-    class="d-flex flex-column justify-center items-center relative my-2">
+  <v-fade-transition :group="true" leave-absolute>
+    <v-sheet v-for="task in tasks" :key="task.text" position="relative" elevation="4" height="100"
+      class="d-flex flex-column justify-center items-center relative my-2">
 
-    <v-btn @click="() => toggleTaskStatus(task)" :class="`${task.open ? 'text-gray' : 'text-green'} right-2 top-2 w-min`"
-      variant="plain" position="absolute" icon="mdi-check-circle-outline" rounded="xl" />
+      <v-btn @click="() => toggleTaskStatus(task)"
+        :class="`${task.open ? 'text-gray' : 'text-green'} right-2 top-2 w-min`" variant="plain" position="absolute"
+        icon="mdi-check-circle-outline" rounded="xl" />
 
-    <p class="text-xl">{{ task.text }}</p>
-  </v-sheet>
+      <p class="text-xl">{{ task.text }}</p>
+    </v-sheet>
+  </v-fade-transition>
 </template>
