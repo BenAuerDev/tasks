6d3766c5137fe70f75e735980cd2f3f1e1a57974
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-07-14 23:15:40 +0200

    add NavigationLink component

---
+++ src/components/navigation/NavigationLink.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  to: string
+  text: string
+}>()
+</script>
+
+<template>
+  <RouterLink
+    class="flex basis-full items-center justify-center justify-self-stretch text-lg transition-transform duration-200 hover:scale-125"
+    :to="to"
+  >
+    {{ text }}
+  </RouterLink>
+</template>
