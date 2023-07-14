e8c57e6c65a2b092cd4d3853861b1d8caa618afc
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-07-14 23:14:55 +0200

    rework navigation and change path
    -  src/components/Navigation.vue
    + src/components/navigation/Navigation.vue
    
    navigation was styled with background, box-shadow and a fixed height
    
    navigation-link was styled with hover, transition-transform and transition-duration
    
    because the styling of the single navigation-link-elements became quite long I extracted it into a separate component and used it in Navigation.vue

---
+++ src/components/navigation/Navigation.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import NavigationLink from './NavigationLink.vue'
+</script>
+
+<template>
+  <nav>
+    <div class="flex h-16 bg-blue-400 shadow-md">
+      <NavigationLink to="/" text="Overview" />
+
+      <v-divider vertical></v-divider>
+
+      <NavigationLink to="/finished" text="Finished" />
+    </div>
+  </nav>
+</template>
