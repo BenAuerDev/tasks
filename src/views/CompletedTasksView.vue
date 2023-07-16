4507350dab71f6554e53353ea3396b63e440bf03
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-07-17 00:35:27 +0200

    rename getFinishedTasks to getCompletedTasks

--- src/views/CompletedTasksView.vue
+++ src/views/CompletedTasksView.vue
@@ -3,11 +3,11 @@ import { storeToRefs } from 'pinia'
 import TaskCardList from '../components/TaskCardList.vue'
 import { useTaskStore } from '../store/task'
 
-const { getFinishedTasks } = storeToRefs(useTaskStore())
+const { getCompletedTasks } = storeToRefs(useTaskStore())
 </script>
 
 <template>
   <div class="my-4 flex w-full max-w-lg flex-col gap-y-2">
-    <TaskCardList :tasks="getFinishedTasks" />
+    <TaskCardList :tasks="getCompletedTasks" />
   </div>
 </template>
