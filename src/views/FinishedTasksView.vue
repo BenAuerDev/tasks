ffc97e43153b8e86032627d4f77c091095a6fc58
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-07-17 00:28:29 +0200

    update FinishedTasksView.vue
    as this view wasn't touched for a while and I removed the individual margin from the task-card-list and added it to Overview.vue I now have to add the same functionality to the div holding the task-card

--- src/views/FinishedTasksView.vue
+++ src/views/FinishedTasksView.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
-import { useTaskStore } from '../store/task'
-import TaskCardList from '../components/TaskCardList.vue'
 import { storeToRefs } from 'pinia'
+import TaskCardList from '../components/TaskCardList.vue'
+import { useTaskStore } from '../store/task'
 
 const { getFinishedTasks } = storeToRefs(useTaskStore())
-
 </script>
 
 <template>
-    <div class="w-full max-w-lg">
-        <TaskCardList :tasks="getFinishedTasks" />
-    </div>
+  <div class="my-4 flex w-full max-w-lg flex-col gap-y-2">
+    <TaskCardList :tasks="getFinishedTasks" />
+  </div>
 </template>
