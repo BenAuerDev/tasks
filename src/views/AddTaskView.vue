aa026eaba1802f4160e1b4b1b3126a64bc2c0a0a
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-07-18 15:32:18 +0200

    change import and template after renaming file

--- src/views/AddTaskView.vue
+++ src/views/AddTaskView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TaskFormModal from '../components/TaskFormModal.vue'
+import TaskForm from '../components/TaskForm.vue'
 </script>
 
 <template>
   <div class="my-4 flex w-full max-w-lg flex-col gap-y-2">
-    <TaskFormModal />
+    <TaskForm />
   </div>
 </template>
