b0ffc527998d24bc4894fdd60c9f9125c0a32c1c
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-07-18 15:29:20 +0200

    create add-task-view

---
+++ src/views/AddTaskView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TaskFormModal from '../components/TaskFormModal.vue'
+</script>
+
+<template>
+  <div class="my-4 flex w-full max-w-lg flex-col gap-y-2">
+    <TaskFormModal />
+  </div>
+</template>
